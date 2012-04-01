12f806c7bde942557e87fdd8046edc150250a250
Ryan Williams <rcw4rcw@gmail.com>
2012-04-01 19:02:16 -0400

    Created the Clases

---
+++ MinecraftEnchantmentCalculator/src/minecraftenchantmentcalculator/Material.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minecraftenchantmentcalculator;
+
+/**
+ *
+ * @author ryanwilliams
+ */
+public enum Material {
+    WOOD,
+    LEATHER,
+    STONE,
+    IRON,
+    CHAIN,
+    DIAMOND,
+    GOLD
+}
