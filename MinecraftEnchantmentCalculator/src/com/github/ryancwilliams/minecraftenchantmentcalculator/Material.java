63e1eec6e0849f87092b4b05737f6e89cee2e55c
Ryan Williams <rcw4rcw@gmail.com>
2012-04-01 19:04:36 -0400

    Renamed the package for Hosting on Github

--- MinecraftEnchantmentCalculator/src/com/github/ryancwilliams/minecraftenchantmentcalculator/Material.java
+++ MinecraftEnchantmentCalculator/src/com/github/ryancwilliams/minecraftenchantmentcalculator/Material.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package minecraftenchantmentcalculator;
+package com.github.ryancwilliams.minecraftenchantmentcalculator;
 
 /**
  *
