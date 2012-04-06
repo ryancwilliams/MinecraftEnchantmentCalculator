a56b6741af151d471c0f0b99533b1411ab08f860
Ryan Williams <rcw4rcw@gmail.com>
2012-04-05 23:21:04 -0400

    added Probability capability to AppliedEnchantment,  Closes #4

--- MinecraftEnchantmentCalculator/src/com/github/ryancwilliams/minecraftenchantmentcalculator/AppliedEnchantment.java
+++ MinecraftEnchantmentCalculator/src/com/github/ryancwilliams/minecraftenchantmentcalculator/AppliedEnchantment.java
@@ -13,32 +13,12 @@ public class AppliedEnchantment {
     public final Enchantment enchantment;
     public final EnchantmentPower power;
     
-    private AppliedEnchantment[] subs;
-    private boolean hasSubs = false;
-    
     public AppliedEnchantment(EnchantmentSet available,Enchantment enchantment,EnchantmentPower power) {
         this.available = available;
         this.enchantment = enchantment;
         this.power = power;
     }
-    public AppliedEnchantment[] getSubs() {
-        return this.subs;
-    }
-    public void applyEnchantments() {
-        this.subs = EnchantmentTools.applyEnchantments(available);
-        this.hasSubs = true;
-    }
-    public void applyEnchantments(int times) {
-        this.applyEnchantments();
-        times--;
-        
-        int length = this.subs.length;
-        
-        for (int c = 0;c < length;c++) {
-            this.subs[c].applyEnchantments(times);
-        }
-    }
-    public boolean hasSubs() {
-        return this.hasSubs;
+    public double getProbability() {
+        return EnchantmentProbabilityTools.getProbability(this.available.mel, this.available.depth, this.available.enchantments, this.enchantment);
     }
 }
